1cc34a8791de894294dcbdfdcf9238574737daf9
jin1963 <kaojino89@gmail.com>
2025-07-17 06:21:59 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const tokenConfig = {
+  KJC: {
+    address: "0xd479ae350dc24168e8db863c5413c35fb2044ecd",
+    symbol: "KJC",
+    decimals: 18,
+    image: "token_logo-1.png"
+  },
+  G3X24: {
+    address: "0x6cfD8Fe423F20F94825b5edB1E94068fBea19dC9",
+    symbol: "G3X24",
+    decimals: 18,
+    image: "token_logo_2.png"
+  },
+  LYDIA: {
+    address: "0x0fa662697d93Eb024f411E416f681EA7FECFcF96",
+    symbol: "LYDIA",
+    decimals: 18,
+    image: "token_logo_3.png"
+  }
+};
